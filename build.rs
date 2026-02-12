1e8680481763f8ec5afbb4276eeef419e665a14e
Harsh Godkar <harsh.godkar.inventyv@gmail.com>
2026-02-12 13:59:10 +0530

    Refactor proto build process and update proto file paths

--- build.rs
+++ build.rs
@@ -1,12 +1,44 @@
+use std::{path::Path, process::Command};
+
 fn main() {
- 
+    let proto_repo_url = "https://github.com/Dynamic-1001-GmbH/proto-schema-management";
+    let proto_dir = "proto";
+
+    clone_proto_repo(proto_repo_url, proto_dir);
     // Compile proto files - note the correct path
     prost_build::Config::new()
         .protoc_arg("--experimental_allow_proto3_optional")
         .out_dir(std::env::var("OUT_DIR").unwrap())
         .compile_protos(
-            &["./proto/user.proto"],  // Updated path
-            &["./proto"]  // Include path
+            &["./proto/crm/user.proto", "./proto/login/hello.proto", "./proto/register/user.proto"],// Updated path
+            &[proto_dir],            // Include path
         )
         .unwrap();
+}
+
+
+fn clone_proto_repo(repo_url: &str, target_dir: &str) {
+    let path = Path::new(target_dir);
+ 
+    // If the proto directory exists, remove it
+    if path.exists() {
+        println!("cargo:warning=Removing existing proto directory...");
+        std::fs::remove_dir_all(path).expect("Failed to remove existing proto directory");
+    }
+ 
+    // Clone the repository
+    println!("cargo:warning=Cloning proto repository from {}...", repo_url);
+    let status = Command::new("git")
+        .args(&["clone", repo_url, target_dir])
+        .status()
+        .expect("Failed to execute git clone command");
+ 
+    if !status.success() {
+        panic!("Failed to clone proto repository from {}", repo_url);
+    }
+ 
+    println!("cargo:warning=Proto repository cloned successfully");
+ 
+    // Tell Cargo to rerun this build script if the proto directory changes
+    println!("cargo:rerun-if-changed={}", target_dir);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,9 @@ pub fn greet(name: &str) -> String {
 pub mod register {
     include!(concat!(env!("OUT_DIR"), "/register.rs"));
 }
+pub mod login {
+    include!(concat!(env!("OUT_DIR"), "/login.rs"));
+}
+pub mod crm {
+    include!(concat!(env!("OUT_DIR"), "/crm.rs"));
+}
